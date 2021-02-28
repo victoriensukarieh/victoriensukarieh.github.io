2b64bab18986605346416b13f2938987c2f95af4
Victorien Sukarieh <vsukarieh@gmail.com>
2021-02-28 21:21:47 +0200

    redesign toggle theme
    use font awesome icon to toggle theme

--- files/js/theme-toggle.js
+++ files/js/theme-toggle.js
@@ -1,8 +1,13 @@
 function toggleTheme() {
     var el = document.getElementById("mainStyle");
+    var icon = document.getElementById("toggleThemeBtn");
     if (el.href.match("files/css/colors.css")) {
         el.href = "files/css/invertcolors.css";
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
     } else {
         el.href = "files/css/colors.css";
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
     }
 }
